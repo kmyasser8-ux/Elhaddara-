c6299c87a431d77c9bf9484e5c150f867d76eb89
kmyasser8-ux <kmyasser8@gmail.com>
2026-05-24 21:33:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-console.log("تم ربط ملف الجافا سكريبت بنجاح!");
-alert("مرحباً بك! المنصة الآن جاهزة للتفاعل.");
-// ربط عناصر القائمة الجانبية من الـ HTML
-const menuToggle = document.getElementById('menuToggle');
-const closeSidebar = document.getElementById('closeSidebar');
-const sidebar = document.getElementById('sidebar');
 
-// عند الضغط على زر ☰ (فتح القائمة)
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.remove('-right-64');
-    sidebar.classList.add('right-0');
-});
-
-// عند الضغط على زر ✕ (إغلاق القائمة)
-closeSidebar.addEventListener('click', () => {
-    sidebar.classList.remove('right-0');
-    sidebar.classList.add('-right-64');
-});
 // ===============================
 // منصة الحضارة | ملف JavaScript
 // ===============================
